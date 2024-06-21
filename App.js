6e41996e463f9e4f39c8ed74f5f0a8d64f86284c
Priyanshu835 <priyanshurai835@gmail.com>
2024-06-21 21:46:26 +0530

    running react in html file without create react app using cdn links

---
+++ App.js
@@ -0,0 +1,6 @@
+const heading= React.createElement("h1",{},"Namaste Everyone");
+const heading1= React.createElement("h2",{},"chlo shuru krte h");
+const heading2= React.createElement("div",{},[heading,heading1]);
+console.log(heading2);
+const root= ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading2);
