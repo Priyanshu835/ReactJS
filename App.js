9c71dcaf4370223feb6e7fcdb8fa7b24601476ed
Priyanshu835 <priyanshurai835@gmail.com>
2024-06-23 16:58:32 +0530

    initiated the app using parcel

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading= React.createElement("h1",{},"Namaste Everyone");
 const heading1= React.createElement("h2",{},"chlo shuru krte h");
 const heading2= React.createElement("div",{},[heading,heading1]);
